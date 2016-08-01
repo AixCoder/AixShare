f1e265f83a6de8ceadde445f1b2ae2fb15af65e8
liuhongnian <liuhongnian16@gmail.com>
2016-08-01 15:46:51 +0800

    项目初始化

---
+++ AixShare/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AixShare
+//
+//  Created by liuhongnian on 16/8/1.
+//  Copyright © 2016年 liuhongnian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AixShare/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AixShare
+//
+//  Created by liuhongnian on 16/8/1.
+//  Copyright © 2016年 liuhongnian. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
